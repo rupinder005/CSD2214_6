b81c8dd474c1f76144553b625dd2350235f37f43
rupinder005 <43041052+rupinder005@users.noreply.github.com>
2019-01-14 17:00:11 -0500

    CLASS ACTIVITY

---
+++ Script.js
@@ -0,0 +1,13 @@
+// Name : Rupinder Kaur
+// Date : January,14,2019
+// Student Id : C0745120
+// Course Code : CSD 2214 Section 6
+
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[name="enddate"]');
+const clock = document.querySelector('#clock');
+const daysSpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const minutesSpan = clock.querySelector('.minutes');
+const secondsSpan = clock.querySelector('.seconds');
